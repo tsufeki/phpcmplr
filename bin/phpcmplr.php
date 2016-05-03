afd9a03c348aada3c3d4ef19a2cf055218f7947f
tsufeki <tsufeki@ymail.com>
2016-05-03 21:01:28 +0200

    Add startup script.

---
+++ bin/phpcmplr.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use PhpCmplr\Server\Server;
+
+$opts = getopt('', ['help', 'port:']);
+if (!is_array($opts)) {
+    $opts = [];
+}
+
+if (array_key_exists('help', $opts)) {
+    echo "PhpCmplr: PHP code auto-completion server.\n";
+    echo "\n";
+    echo "Options:\n";
+    echo "    --port=<port> Port to listen on.\n";
+    echo "    --help        Print this help message and exit.\n";
+    exit(0);
+}
+
+if (!array_key_exists('port', $opts)) {
+    echo "Required option not provided: --port\n";
+    exit(2);
+}
+
+$server = new Server((int)$opts['port']);
+$server->run();
+
